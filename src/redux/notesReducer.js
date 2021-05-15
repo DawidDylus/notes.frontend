c7b4d1e117aa327e5b50ab8223e9d6960e5a6755
Dawid Dylus <36481644+DawidDylus@users.noreply.github.com>
2021-05-15 18:01:26 +0200

    Creating custom reducer for notes
    Co-Authored-By: Adkins1 <14940128+Adkins1@users.noreply.github.com>

---
+++ src/redux/notesReducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+    notes: [],
+}
+
+// Enum with action names?
+export const ActionTypes = {
+    SET_NOTES: 'SET_NOTES',
+}
+
+// Constructor for all actions and defining what properties they take
+export const ActionCreators = {
+    // Payload is anything you push to this function (like function property)
+    setNotes: payload => ({ type: ActionTypes.SET_NOTES, payload }),
+
+}
+
+// How the action works
+// NOTE only one state could be called at the time (as an enum can)
+export default function NotesReducer(state = initialState, action) {
+    switch (action.type) {
+        // If SET_NOTES action will be called in ActionCreators, this action create array of notes and fill it with the payload(notes)
+        case ActionTypes.SET_NOTES:
+            return { ...state, notes: [...action.payload] }
+        default:
+            return state;
+    }
+}
